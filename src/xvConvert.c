d7e5e5aa20c220cbba049d7c9c59bcc3eaef06ef
sndnvaps <sndnvaps@gmail.com>
2018-03-11 12:01:33 +0800

    From 4751a9bc99ca67f8c761105e0b87804de07dcf8a Mon Sep 17 00:00:00 2001 From: ZHANG Biao <bbbiao@gmail.com> Date: Wed, 3 Dec 2014 17:14:00 +0800 Subject: [PATCH] bugfix, support the new xv format: firt 0x200000 bytes maybe

--- src/xvConvert.c
+++ src/xvConvert.c
@@ -1,122 +1,144 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include <malloc.h>
 
-
 // reference: http://www.cnblogs.com/lsjwq/archive/2011/05/15/2046716.html
 
-int XV_Convert(char* src_file) {
-    char *fb, peek[5];
-    int mn, type, l, c, fail;
-    size_t rl;
-    char *ts=NULL;
-    char *video_type[7] = {"rmvb", "wmv", "flv", "avi", "mp4", "mpg", "mkv"};
-    char val[7]={46, 48, 70, 82, 0, 0, 26};
-    int j;
-    FILE *fin, *fout;
-    char dst_file[255]={0};
+#define VIDEO_TYPE_NUM (7)
+#define FILENAME_MAX_LEN (255)
+#define SKIP_HEAD_LEN (0x200000)
+#define REPORT_DATA_LEN (0x100000)
+#define BUFFER_LEN (0x40000)
 
-    fb = (char*)malloc(sizeof(char) * 0x200011);
-    c = 0;
-	fail = 0;
+int XV_Convert(char* src_file) {
+    char *fb = NULL, peek[5];
+    int mn = 0, type = 0, l = 0, c = 0, fail = 0;
+	size_t rl = 0;
+    char *ts = NULL;
+    char *video_type[VIDEO_TYPE_NUM] = {"rmvb", "wmv", "flv", "avi", "mp4", "mpg", "mkv"};
+    char val[VIDEO_TYPE_NUM] = {46, 48, 70, 82, 0, 0, 26};
+    int j = 0;
+    FILE *fin = NULL, *fout = NULL;
+    char dst_file[FILENAME_MAX_LEN] = {0};
+    int translate_percentage = 0;
 
     fin = fopen(src_file, "rb");
-    if (fin==NULL) return -1;
+    if (fin == NULL) {
+        return -1;
+    }
+
+    fb = (char*)malloc(SKIP_HEAD_LEN);
+    c = 0;
+
     fseek(fin, 0, SEEK_END);
     l = ftell(fin);
     fseek(fin, 0, SEEK_SET);
-    fread(fb, 1, 0x200000, fin);
+    fread(fb, 1, SKIP_HEAD_LEN, fin);
+
+    for (size_t byte_count = 0; byte_count < SKIP_HEAD_LEN; byte_count += sizeof(int)) {
+        int* int_ptr = (int*)(fb + byte_count);
+        if (*int_ptr != 0) {
+            fseek(fin, 0, SEEK_SET);
+            break;
+        }
+    }
+
     fread(peek, 1, 4, fin);
 
-    mn = ('R'-peek[1] + 0x100) & 0xFF;
-    if (((peek[2]+mn) & 0xFF) == 'M' && ((peek[3]+mn) & 0xFF ) == 'F') {
+    mn = ('R' - peek[1] + 0x100) & 0xFF;
+    if (((peek[2] + mn) & 0xFF) == 'M' && ((peek[3] + mn) & 0xFF ) == 'F') {
         type = 1;
         goto ok;
     }
-    mn = (0x26-peek[1] + 0x100) & 0xFF;
-    if (((peek[2]+mn) & 0xFF) == 0xB2 && ((peek[3]+mn) & 0xFF) == 0x75) {
+    mn = (0x26 - peek[1] + 0x100) & 0xFF;
+    if (((peek[2] + mn) & 0xFF) == 0xB2 && ((peek[3] + mn) & 0xFF) == 0x75) {
         type = 2;
         goto ok;
     }
-    mn = ('L'-peek[1] + 0x100) & 0xFF;
-    if (((peek[2]+mn) & 0xFF) == 'V') {
+    mn = ('L'- peek[1] + 0x100) & 0xFF;
+    if (((peek[2] + mn) & 0xFF) == 'V') {
         type = 3;
         goto ok;
     }
-    mn = ('I'-peek[1] + 0x100) & 0xFF;
-    if (((peek[2]+mn) & 0xFF) == 'F' && ((peek[3]+mn) & 0xFF) == 'F') {
+    mn = ('I' - peek[1] + 0x100) & 0xFF;
+    if (((peek[2] + mn) & 0xFF) == 'F' && ((peek[3] + mn) & 0xFF) == 'F') {
         type = 4;
         goto ok;
     }
     mn = (-peek[1] + 0x100) & 0xFF;
-    if (((peek[2]+mn) & 0xFF) == 0) {
+    if (((peek[2] + mn) & 0xFF) == 0) {
         type = 5;
         goto ok;
     }
     mn = (-peek[1] + 0x100) & 0xFF;
-    if (((peek[2]+mn) & 0xFF) == 1 && ((peek[3]+mn) & 0xFF) == 0xBA) {
+    if (((peek[2] + mn) & 0xFF) == 1 && ((peek[3] + mn) & 0xFF) == 0xBA) {
         type = 6;
         goto ok;
     }
-    mn = (0x45-peek[1] + 0x100) & 0xFF;
-    if (((peek[2]+mn) & 0xFF) == 0xDF && ((peek[3]+mn) & 0xFF) == 0xA3) {
+    mn = (0x45 - peek[1] + 0x100) & 0xFF;
+    if (((peek[2] + mn) & 0xFF) == 0xDF && ((peek[3] + mn) & 0xFF) == 0xA3) {
         type = 7;
         goto ok;
     }
 
+//SEND_REPORT:
     strcpy(dst_file, src_file);
-    strcat(dst_file, ".new");
+    strcat(dst_file, ".report");
     fout = fopen(dst_file, "wb");
-    if (fout==NULL) { fclose(fin); return -2; }
-    fwrite(fb, 0, 0x200000, fout);
-    fwrite(fb, 0, 4, fout);
+    if (fout == NULL) {
+        fclose(fin);
+        return -2;
+    }
+    fwrite(fb, 0, SKIP_HEAD_LEN, fout);
+    fwrite(peek, 0, 4, fout);
 
-    fread(fb, 0, 0x100000, fin);
-    fwrite(fb, 0, 0x100000, fout);
+    fread(fb, 0, REPORT_DATA_LEN, fin);
+    fwrite(fb, 0, REPORT_DATA_LEN, fout);
     fail += 1;
     fclose(fin);
     fclose(fout);
-    printf("convert failed\n");
+    fprintf(stderr, "convert failed, please send %s to bbbiao@gmail.com\n", dst_file);
     free(fb);
     return -1;
 
 ok:
-    ts = video_type[type-1];
+    ts = video_type[type - 1];
     strcpy(dst_file, src_file);
     strcat(dst_file, ".");
     strcat(dst_file, ts);
     fout = fopen(dst_file, "wb");
-    peek[0] = val[type-1];
-    for (j=1; j<=3; j++) {
-        peek[j] = (peek[j]+mn) & 0xFF;
+    peek[0] = val[type - 1];
+    for (j = 1; j <= 3; j++) {
+        peek[j] = (peek[j] + mn) & 0xFF;
     }
     fwrite(peek, 1, 4, fout);
     fread(fb, 1, 0x3FC, fin);
-    for (j=1; j<=0x3FC; j++) {
-        fb[j-1] = (fb[j-1]+mn) & 0xFF;
+    for (j = 1; j <= 0x3FC; j++) {
+        fb[j - 1] = (fb[j - 1]+mn) & 0xFF;
     }
     fwrite(fb, 1, 0x3FC, fout);
-    printf("go to ");
     while (ftell(fin) <= l ) {
-
         rl = fread(fb, 1, 0x40000, fin);
         if (rl == 0) break;;
         fwrite(fb, 1, rl, fout);
         c = c + 1;
         if (c == 10) {
             double pos = ftell(fout);
-            pos = pos / (l-0x200000);
+            pos = pos / (l - 0x200000);
             pos = pos * 100;
-            printf("%d ", (int)pos);
+            if ((int)pos > translate_percentage) {
+                printf("Converting %3d%%\n", (int)pos);
+                translate_percentage = (int)pos;
+            }
             c = 0;
         }
     }
-    printf("100\n");
     fclose(fin);
     fclose(fout);
     free(fb);
-    
+
     return 0;
 }
 
@@ -127,10 +149,12 @@ void usage(char *filename) {
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2) {
+	if (argc != 2) {
 		usage(argv[0]);
-        return 0;
-    }
+		return 0;
+	}
 
-    return XV_Convert(argv[1]);
+	return XV_Convert(argv[1]);
+
 }
+
